7c6a0ed144cda4fe115d5082c92432e250299b56
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-05 18:58:23 +0200

    fix exit shell implementation

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER
+#define HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int _strlen(char *s);
+
+#endif
